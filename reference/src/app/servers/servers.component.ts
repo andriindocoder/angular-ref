b1300b310ef3cadfc5c6e927a6a3c0cb12c1f141
andri <andriwicaksonost@gmail.com>
2019-10-13 21:00:21 +0700

    Event Binding when clicked

--- reference/src/app/servers/servers.component.ts
+++ reference/src/app/servers/servers.component.ts
@@ -7,15 +7,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./servers.component.css']
 })
 export class ServersComponent implements OnInit {
-
   allowNewServer = false;
+  serverCreationStatus = 'No server was created';
+
   constructor() {
     setTimeout(() => {
       this.allowNewServer = true;
     }, 2000);
-   }
-
-  ngOnInit() {
   }
 
+  onCreateServer() {
+    this.serverCreationStatus = 'Server has been created';
+  }
+
+  ngOnInit() {}
 }
