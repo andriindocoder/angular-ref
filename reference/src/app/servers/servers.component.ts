7eddcf46a433ee2b3846bc13b9edf349b668f73f
andri <andriwicaksonost@gmail.com>
2019-10-13 20:47:17 +0700

    Selector as class
    Command K Command F buat ngerapihin

--- reference/src/app/servers/servers.component.ts
+++ reference/src/app/servers/servers.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: '[app-servers]',
+  // tslint:disable-next-line: component-selector
+  selector: '.app-servers',
   // templateUrl: './servers.component.html',
   template: `<app-server></app-server>`,
   styleUrls: ['./servers.component.css']
