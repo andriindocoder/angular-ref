74222f4e18b9ddce5130c0edeed8bd5876bbafec
andri <andriwicaksonost@gmail.com>
2019-10-20 12:19:56 +0700

    ReactiveFormsModule

--- reference/src/app/app.component.ts
+++ reference/src/app/app.component.ts
@@ -1,5 +1,5 @@
-import { Component, ViewChild } from '@angular/core';
-import { NgForm } from '@angular/forms';
+import { Component } from '@angular/core';
+import { FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -7,37 +7,6 @@ import { NgForm } from '@angular/forms';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  @ViewChild('f', { static: true }) signUpForm: NgForm;
-  defaultQuestion = 'pet';
-  answer = '';
   genders = ['Male', 'Female'];
-  user = {
-    email: '',
-    name: '',
-    secret: '',
-    gender: '',
-    answer: ''
-  };
-  submitted = false;
-
-
-  suggestUserName() {
-    const suggestedName = 'Superuser';
-    // this.signUpForm.setValue({});
-    this.signUpForm.form.patchValue({
-      userData: {
-        username: suggestedName
-      }
-    });
-  }
-
-  onSubmit() {
-    console.log(this.signUpForm);
-    this.submitted = true;
-    this.user.name = this.signUpForm.value.userData.username;
-    this.user.email = this.signUpForm.value.userData.email;
-    this.user.secret = this.signUpForm.value.secret;
-    this.user.answer = this.signUpForm.value.questionAnswer;
-    this.user.gender = this.signUpForm.value.gender;
-  }
+  signupForm = FormGroup;
 }
