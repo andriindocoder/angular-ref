c5b0a97040715c139bc52d905d581d7c52433794
andri <andriwicaksonost@gmail.com>
2019-10-20 08:31:30 +0700

    Basic files forms

--- reference/src/app/app.component.ts
+++ reference/src/app/app.component.ts
@@ -6,5 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'reference';
+  suggestUserName() {
+    const suggestedName = 'Superuser';
+  }
 }
