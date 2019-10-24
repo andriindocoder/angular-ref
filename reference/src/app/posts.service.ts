ce1d03d43b02e9e98347144b411047001858a3bd
andri <andriwicaksonost@gmail.com>
2019-10-24 17:43:34 +0700

    Setting Headers

--- reference/src/app/posts.service.ts
+++ reference/src/app/posts.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Post } from './post.model';
 import { map, catchError } from 'rxjs/operators';
 import { Subject, throwError } from 'rxjs';
@@ -31,7 +31,11 @@ export class PostsService {
 
   fetchPosts() {
     return this.http
-      .get<{ [key: string]: Post }>('https://ng-max.firebaseio.com/posts.json')
+      .get<{ [key: string]: Post }>('https://ng-max.firebaseio.com/posts.json', {
+        headers: new HttpHeaders({
+          'Custom-Header' : 'Hola'
+        })
+      })
       .pipe(
         map(responseData => {
           const postsArray: Post[] = [];
