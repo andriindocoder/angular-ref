d15bebf607f648ce1894c7058c443eb94c5b74d6
andri <andriwicaksonost@gmail.com>
2019-10-28 11:02:52 +0700

    Country Detail

--- reference/src/app/country-detail/country-detail.component.ts
+++ reference/src/app/country-detail/country-detail.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Country } from '../country';
+import { ActivatedRoute } from '@angular/router';
+import { CountryService } from '../country.service';
+import { Location } from '@angular/common';
 
 @Component({
   selector: 'app-country-detail',
@@ -6,10 +10,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./country-detail.component.css']
 })
 export class CountryDetailComponent implements OnInit {
+  country: Country;
 
-  constructor() { }
+  constructor(
+    private route: ActivatedRoute,
+    private countryService: CountryService,
+    private location: Location
+  ) {}
 
   ngOnInit() {
+    this.getCountry();
   }
 
+  getCountry(): void {
+    const name: string = this.route.snapshot.paramMap.get('name');
+    this.country = this.countryService.getCountry(name);
+  }
+
+  goBack(): void {
+    this.location.back();
+  }
 }
