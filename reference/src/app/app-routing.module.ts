dc61a6028b8bc075ae779d53a0f78e44b454644b
andri <andriwicaksonost@gmail.com>
2019-10-28 11:21:02 +0700

    Finishing Up

--- reference/src/app/app-routing.module.ts
+++ reference/src/app/app-routing.module.ts
@@ -1,8 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { CountryDetailComponent } from './country-detail/country-detail.component';
+import { AllCountriesComponent } from './all-countries/all-countries.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: 'home', component: HomeComponent },
+  { path: 'detail/:name', component: CountryDetailComponent},
+  { path: 'all-countries', component: AllCountriesComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
