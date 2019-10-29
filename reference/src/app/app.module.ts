826c938955aea47873c327ff97ccb70fa749f0bc
andri <andriwicaksonost@gmail.com>
2019-10-29 16:18:37 +0700

    Routing Generik

--- reference/src/app/app.module.ts
+++ reference/src/app/app.module.ts
@@ -8,19 +8,22 @@ import { TopBarComponent } from './top-bar/top-bar.component';
 import { ProductListComponent } from './product-list/product-list.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { ProductAlertsComponent } from './product-alerts/product-alerts.component';
+import { ProductDetailsComponent } from './product-details/product-details.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     TopBarComponent,
     ProductListComponent,
-    ProductAlertsComponent
+    ProductAlertsComponent,
+    ProductDetailsComponent
   ],
   imports: [
     BrowserModule,
     ReactiveFormsModule,
     RouterModule.forRoot([
-      { path: '', component: ProductListComponent}
+      { path: '', component: ProductListComponent },
+      { path: 'products/:id', component: ProductDetailsComponent }
     ]),
     BrowserAnimationsModule
   ],
