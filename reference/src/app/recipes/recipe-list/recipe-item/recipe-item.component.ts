fa1a01a1d8b64519ffad964399c61b9d34748d0a
Andri Sedania <andri@sedaniainnovator.com>
2019-11-10 18:45:16 +0700

    How Components Bind
    Cara component berkomunikasi

--- reference/src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
+++ reference/src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, EventEmitter, Output } from '@angular/core';
+
+import { Recipe } from '../../recipe.model';
 
 @Component({
   selector: 'app-recipe-item',
@@ -6,10 +8,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-item.component.css']
 })
 export class RecipeItemComponent implements OnInit {
+  @Input() recipe: Recipe; // To get data from outside
+  @Output() recipeSelected = new EventEmitter<void>(); //Can listen from outside
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  onSelected() {
+    this.recipeSelected.emit();
   }
 
 }
