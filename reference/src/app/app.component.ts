2e1b5b1a1d09f9a974c982e6bcd7455b343b1010
Andri Sedania <andri@sedaniainnovator.com>
2019-11-15 15:39:50 +0700

    Sweet Alert installation

--- reference/src/app/app.component.ts
+++ reference/src/app/app.component.ts
@@ -1,10 +1,48 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'reference';
+
+  ngOnInit() {
+    Swal.mixin({
+      input: 'text',
+      confirmButtonText: 'Next &rarr;',
+      showCancelButton: true,
+      progressSteps: ['1', '2', '3']
+    }).queue([
+      {
+        title: 'Question 1',
+        text: 'Chaining swal2 modals is easy'
+      },
+      {
+        title: 'Pertanyaan 2',
+        input: 'select',
+        inputOptions: {
+          apples: 'Apples',
+          bananas: 'Bananas',
+          grapes: 'Grapes',
+          oranges: 'Oranges'
+        },
+      },
+      'Question 3'
+    ]).then((result) => {
+      if (result.value) {
+        const answers = JSON.stringify(result.value)
+        Swal.fire({
+          title: 'All done!',
+          html: `
+            Your answers:
+            <pre><code>${answers}</code></pre>
+          `,
+          confirmButtonText: 'Lovely!'
+        })
+      }
+    })
+  }
 }
