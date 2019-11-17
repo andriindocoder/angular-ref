95dbc2c38838b0186f79bb06fc2c44664ea9d40d
Andri Sedania <andri@sedaniainnovator.com>
2019-11-17 12:52:53 +0700

    Interpolation 5 - Create Test Component
    inner template
    Di template bisa pakai javascript property atau function

---
+++ reference/src/app/test/test.component.ts
@@ -0,0 +1,28 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-test',
+  template: `
+    <h2>Hello {{ name }}</h2>
+    <h2>{{ 2+2 }}</h2>
+    <h2>{{ name.length }}</h2>
+    <h2>{{ name.toUpperCase() }}</h2>
+    <h2>{{ greetUser() }}</h2>
+    <h2>{{ siteUrl }}</h2>
+  `,
+  styles: []
+})
+export class TestComponent implements OnInit {
+  public name = 'Indocoder';
+  public siteUrl = window.location.href;
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  greetUser() {
+    return 'Hello ' + this.name;
+  }
+
+}
