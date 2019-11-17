8fc4859b970d3c70ce5a0ac5571c9df1821d3e8a
Andri Sedania <andri@sedaniainnovator.com>
2019-11-17 13:19:05 +0700

    8 - Style Binding
    Mirip dengan Class Binding

--- reference/src/app/test/test.component.ts
+++ reference/src/app/test/test.component.ts
@@ -4,31 +4,19 @@ import { Component, OnInit } from '@angular/core';
   selector: 'app-test',
   template: `
     <h2>Hello {{ name }}</h2>
-    <h2 class="text-special" [class]="successClass">Indocoder</h2>
-    <h2 [class.text-danger]="hasError">Indocoder</h2>
-    <h2 [ngClass]="messageClasses">Indocoder</h2>
+    <h2 [style.color]="hasError ? 'red' : 'green'">Indocoder</h2>
+    <h2 [style.color]="highlightColor">Indocoder</h2>
+    <h2 [ngStyle]="titleStyles">Indocoder</h2>
   `,
-  styles: [`
-    .text-success {
-      color: green;
-    }
-    .text-danger {
-      color: red;
-    }
-    .text-special {
-      font-style: italic;
-    }
-  `]
+  styles: []
 })
 export class TestComponent implements OnInit {
   public name = 'Indocoder';
-  public successClass = 'text-success';
-  public hasError = false;
-  public isSpecial = true;
-  public messageClasses = {
-    'text-success': !this.hasError,
-    'text-danger' : this.hasError,
-    'text-special': this.isSpecial
+  public hasError = true;
+  public highlightColor = 'orange';
+  public titleStyles = {
+    color: 'blue',
+    fontStyle: 'italic'
   };
 
   constructor() { }
