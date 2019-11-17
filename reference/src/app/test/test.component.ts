286f2afb43672e2752b078f70e0cadce4cc6a07c
Andri Sedania <andri@sedaniainnovator.com>
2019-11-17 13:10:37 +0700

    7 - Class Binding
    Very Useful Karena bisa merubah berdasarkan property state
    Gunakan ngClass kalau banyak pilihannya. Masukannya object

--- reference/src/app/test/test.component.ts
+++ reference/src/app/test/test.component.ts
@@ -4,16 +4,32 @@ import { Component, OnInit } from '@angular/core';
   selector: 'app-test',
   template: `
     <h2>Hello {{ name }}</h2>
-    <input [disabled]="isDisabled" [id]="myId" type="text" value="Indocoder" />
-    <input [disabled]="!isDisabled" id="{{myId}}" type="text" value="Indocoder" />
-    <input bind-disabled="isDisabled" id="{{myId}}" type="text" value="Indocoder" />
+    <h2 class="text-special" [class]="successClass">Indocoder</h2>
+    <h2 [class.text-danger]="hasError">Indocoder</h2>
+    <h2 [ngClass]="messageClasses">Indocoder</h2>
   `,
-  styles: []
+  styles: [`
+    .text-success {
+      color: green;
+    }
+    .text-danger {
+      color: red;
+    }
+    .text-special {
+      font-style: italic;
+    }
+  `]
 })
 export class TestComponent implements OnInit {
   public name = 'Indocoder';
-  public myId = 'testId';
-  public isDisabled = true;
+  public successClass = 'text-success';
+  public hasError = false;
+  public isSpecial = true;
+  public messageClasses = {
+    'text-success': !this.hasError,
+    'text-danger' : this.hasError,
+    'text-special': this.isSpecial
+  };
 
   constructor() { }
 
