375dd01979ebbe390adab13b4e2997c06b43c984
Andri Sedania <andri@sedaniainnovator.com>
2019-11-23 10:00:45 +0700

    24 - Wild Card and Redirecting Routes

--- reference/src/app/app.module.ts
+++ reference/src/app/app.module.ts
@@ -8,13 +8,15 @@ import { TestComponent } from './test/test.component';
 import { EmployeeDetailComponent } from './employee-detail/employee-detail.component';
 import { EmployeeService } from './employee.service';
 import { HttpClientModule } from '@angular/common/http';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     TestComponent,
     EmployeeDetailComponent,
-    routingComponents
+    routingComponents,
+    PageNotFoundComponent
   ],
   imports: [
     BrowserModule,
