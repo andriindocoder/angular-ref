307c4806faaf2b7e17b338e86ab9eb7e25546ef2
Andri Sedania <andri@sedaniainnovator.com>
2020-02-05 10:01:46 +0700

    Piping

--- reference/src/app/app.component.ts
+++ reference/src/app/app.component.ts
@@ -6,5 +6,26 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'reference';
+  name = 'Angular';
+
+  userList = [
+    { name: 'SPA', points: 1000 },
+    { name: 'HTML', points: 850 },
+    { name: 'Angular', points: 12250 },
+    { name: 'jQuery', points: 1123 },
+    { name: 'C Sharp', points: 85 },
+    { name: 'Java', points: 1187 },
+    { name: 'Javascript', points: 8150 },
+    { name: 'Fortran', points: 340 },
+    { name: 'F Sharp', points: 8150 },
+    { name: 'CSS', points: 8150 }
+  ];
+
+  getTitle(points) {
+    if (points > 1000) {
+      return 'beginner';
+    } else {
+      return 'expert';
+    }
+  }
 }
