20a1cf429cf6feb401af1d2abc3bda19fa09e535
Andri Wicaksono <andriwicaksonost@gmail.com>
2020-09-04 18:51:46 +0700

    Tour of Heroes Introduction

--- reference/toh/e2e/src/app.e2e-spec.ts
+++ reference/toh/e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('reference app is running!');
+    expect(page.getTitleText()).toEqual('toh app is running!');
   });
 
   afterEach(async () => {
